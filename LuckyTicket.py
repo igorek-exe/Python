571efa33a91f52b70b7a74fcec164774d4aa1582
igorek-exe <igorek.exe@mail.ru>
2022-03-02 17:13:53 +0300

    изменил имена переменных

--- LuckyTicket.py
+++ LuckyTicket.py
@@ -1,16 +1,16 @@
 nomer = int(input())
 
-r = nomer % 1000
-a = r % 10
-a1 = r // 10
+right = nomer % 1000
+a = right % 10
+a1 = right // 10
 b = a1 % 10
 b1 = a1 // 10
 c = b1 % 10
 resultR = a + b + c
 
-l = nomer // 1000
-d = l % 10
-d1 = l // 10
+left = nomer // 1000
+d = left % 10
+d1 = left // 10
 e = d1 % 10
 e1 = d1 // 10
 f = e1 % 10
